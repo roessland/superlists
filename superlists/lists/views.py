dc19a1c78bfd0149dd7c7c87ea68b7f7a20f6704
Andreas Røssland <andreas.roessland@gmail.com>
2014-02-09 21:32:26 +0000

    Refactor and move to new URL

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
-    if request.method == 'POST' and request.POST.get('item-text'):
-        Item.objects.create(text=request.POST.get('item-text'))
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, "lists/home.html")
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, "lists/list.html", {'items': items})
+
+def new_list(request):
+    if request.method == 'POST' and request.POST.get('item-text'):
+        Item.objects.create(text=request.POST.get('item-text'))
+    return redirect('/lists/the-only-list-in-the-world/')
