f1f6a88a21b359dbf556e285f820b366dfa0cfdc
Andreas Røssland <andreas.roessland@gmail.com>
2014-02-09 18:00:08 +0000

    Refactor home page view to use a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,43 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
 
-browser = webdriver.PhantomJS()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
 
-assert "Django" in browser.title
+    def setUp(self):
+        self.browser = webdriver.PhantomJS()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith visits the front page
+        self.browser.get("http://localhost:8000")
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id("new-item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+
+        # She types "Buy peacock feathers" into a text box
+        inputbox.send_keys("Buy peacock feathers")
+
+        # When she hits enters, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id("list-table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1: Buy peacock feathers" for row in rows)
+        )
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly"
+        self.fail("Finish the test!")
+
+        # The page updates again, and now shows both items on her list
+        # ...
