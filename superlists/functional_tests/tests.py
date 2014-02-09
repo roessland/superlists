bc60bd1024b5eba4995e318bda900ac400a0ce3a
Andreas Røssland <andreas.roessland@gmail.com>
2014-02-09 20:21:57 +0000

    Expand functional test

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -27,12 +27,13 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('new-item')
         self.assertEqual(inputbox.get_attribute('placeholder'), "Enter a to-do item")
 
-        # She types "Buy peacock feathers" into a text box
+        # She types "Buy peacock feathers" into a text box and presses enter.
+        # The pages updates to a new URL, and she can see "1: Buy peacock
+        # feathers" as an item in a to-do list table
         inputbox.send_keys("Buy peacock feathers")
-
-        # When she hits enters, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. She
@@ -45,9 +46,29 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
+        # Now a new user, Francis comes along to the site
 
+        ## Open a new browser session to make sure Edith's session is gone
+        self.browser.quit()
+        self.browser = webdriver.PhantomJS()
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated an unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail("Finish the test!")
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+
+        # Francis starts a new list by entering a new item
+        inputbox = self.browser.find_element_by_id("new-item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets his how unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trance of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
